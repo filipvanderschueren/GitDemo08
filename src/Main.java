bacce19a516d7713d6b6058a632b9decfc524ce9
Filip Van der Schueren <filip.vanderschueren@uantwerpen.be>
2019-04-28 22:33:23 +0200

    class Message added

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,9 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello Felipe's World!");
+
+        Message m = new Message("en de groetjes...");
+        System.out.println(m);
+
     }
 }
