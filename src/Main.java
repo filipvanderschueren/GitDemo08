62ea43f35af970b564d4bde30554c9758cfc8059
Filip Van der Schueren <filip.vanderschueren@uantwerpen.be>
2019-04-28 23:38:23 +0200

    class Student added, main changed

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,9 @@ public class Main {
         Message m = new Message("dit bericht wordt gewijzigd in: Hallo Filip...");
         System.out.println(m.toString());
 
+        Student s = new Student("Jefke");
+        System.out.println("Student added : " + s.getName());
+
+
     }
 }
