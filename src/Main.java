e2df17bdc253ecb06f4bfac15323a0d01222a935
Filip Van der Schueren <filip.vanderschueren@uantwerpen.be>
2019-04-28 22:52:14 +0200

    toString() added

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Felipe's World!");
 
-        Message m = new Message("en de groetjes...");
-        System.out.println(m);
+        Message m = new Message("dit bericht wordt gewijzigd in: Hallo Filip...");
+        System.out.println(m.toString());
 
     }
 }
